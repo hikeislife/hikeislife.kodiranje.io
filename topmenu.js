fb59e6c107ea776ca1542fc97f82e7b6b2b7af47
whatnow <hikeislife@users.noreply.github.com>
2018-04-08 00:05:41 +0200

    fixed top nav and some other edits

---
+++ topmenu.js
@@ -0,0 +1,13 @@
+var topMenu = document.getElementById("header-bottom");
+var smallDevice = window.innerWidth;
+
+window.onscroll = function() {bindMenu()};
+
+function bindMenu() {
+  if (document.body.scrollTop >= 70 || document.documentElement.scrollTop >= 70) {
+    topMenu.className = "fixed-top";
+  }
+  else {
+    topMenu.className = "";
+  }
+}
