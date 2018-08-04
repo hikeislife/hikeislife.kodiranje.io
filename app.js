1520be475f9bcedb05c6edf9f2f3042dbef4f94a
whatnow <hikeislife@users.noreply.github.com>
2018-08-04 21:18:46 +0200

    fixed issues with mob menu

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ if ('serviceWorker' in navigator) { // browser support check
 // adding mobile menu to the cog click
 const mobMenu = document.querySelector("#mobile-cog").addEventListener("click", loadMenu);
 
+window.onmouseup = function() {
+  const hide = document.querySelector("#side-nav");
+  hide.style.display = "none";
+}
 function loadMenu() {
  let menuStatus = document.getElementById("side-nav"),
   menuList = document.getElementsByTagName("li"),
@@ -29,21 +33,9 @@ function loadMenu() {
   menuStatus.style.display = "none";
  }
  else {
-  menuStatus.style.zIndex = "999";
   menuStatus.style.display = "block";
-  menuStatus.style.top = "5px";
-  menuStatus.style.position = "fixed";
-  for (let i = 0; i < menuLength; i ++) {
-   menuList[i].style.boxShadow ="0 1px 0px 0 rgba(0, 0, 0, 0.3), 0 0px 2px 0 rgba(0, 0, 0, 0.3)";
-   menuList[i].style.padding = "5px";
-   menuList[i].style.backgroundColor = "#e5e5e5";
-   menuList[i].style.listStyleType = "none";
-  }
-  menuList[0].style.borderRadius = "2px 2px 0 0";
-  menuList[menuLength-1].style.borderRadius = "0 0 2px 2px";
  }
 }
-
 
 // fixes top bar to the top of the page once scrolled pass logo
 window.onscroll = function() {bindMenu()};
