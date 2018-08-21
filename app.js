942f62f1bb2ccc64ad48b73d20887778d426f853
whatnow <hikeislife@users.noreply.github.com>
2018-08-21 19:38:29 +0200

    multiple edits

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 // registers service worker
-/*if ('serviceWorker' in navigator) { // browser support check
+if ('serviceWorker' in navigator) { // browser support check
   navigator.serviceWorker.register('sw.js')
   .then(function(reg) {
     // run if registration worked
+    if(!navigator.serviceWorker.controller) {
+      return;
+    }
+    if(reg.waiting) {
+      function initializeUI() {
+        reg.pushManager.getSubscription()
+        .then(function(subscription) {
+          isSubscribed = !(subscription === null);
+
+          if (isSubscribed) {
+            console.log('User IS subscribed.');
+          } else {
+            console.log('User is NOT subscribed.');
+          }
+
+        updateBtn();
+        });
+      }
+    }
+
+    function updateBtn() {
+      if (isSubscribed) {
+        pushButton.textContent = 'Disable Push Messaging';
+      } else {
+        pushButton.textContent = 'Enable Push Messaging';
+      }
+
+      pushButton.disabled = false;
+    }
+
+    initializeUI();
+
     if(reg.installing) {
           console.log('Service worker installing');
         } else if(reg.waiting) {
@@ -15,19 +47,23 @@
     // run if registration failed
     console.log('Registration failed with ' + error);
   });
-}*/
+}
+
+
+
 
 // adding mobile menu to the cog click
 const mobMenu = document.querySelector("#mobile-cog").addEventListener("click", loadMenu);
 
+// hides menu on outside click:
 window.onmouseup = function() {
   const hide = document.querySelector("#side-nav");
   hide.style.display = "none";
 }
+
+// shows / hides mobile menu on cog click
 function loadMenu() {
- let menuStatus = document.getElementById("side-nav"),
-  menuList = document.getElementsByTagName("li"),
-  menuLength = menuList.length; 
+ let menuStatus = document.getElementById("side-nav");
   
  if (menuStatus.style.display.match("block")) {
   menuStatus.style.display = "none";
@@ -37,25 +73,9 @@ function loadMenu() {
  }
 }
 
-// fixes top bar to the top of the page once scrolled pass logo
-window.onscroll = function() {bindMenu()};
 
-function bindMenu() {
- let topMenu = document.getElementById("header-bottom"),
-  smallDevice = window.innerWidth;
-   if (document.body.scrollTop >= 70 || document.documentElement.scrollTop >= 70) {
-      topMenu.className = "fixed-top";
-   }
-   else {
-      topMenu.className = "";
-   }
-}
 
-// adds copy/date to the footer
-window.onload = function copid() {
- let d = new Date().getFullYear();
- document.querySelector('footer').innerHTML = "&copy; ČĎŠ " + d;
-}
+
 
 // content loader...
 let xhr = new XMLHttpRequest();
@@ -85,49 +105,212 @@ function pageInit() {
  //getXMP(contents);
 }
 
+
+
+/*
+ scrolls the page back to top on link click as it otherwise loads
+ the content staying at the same scroll pos it was at before the click
+*/
+function backToTop() {
+ const toTop = Array.from(document.querySelectorAll("a[rel$='chapter']"));
+ for (topSide in toTop) {
+  toTop[topSide].onclick = () => window.scrollTo(0, 0);
+ }
+}
+
+
+
+// highlights active side nav item
+function setActive(frag) {
+ var links = document.querySelectorAll("#side-nav a");
+ for (let i = 0; i < links.length; i++) {
+  if (links[i].getAttribute("href").substr(1) === frag) {
+   links[i].firstChild.setAttribute("class", "active");
+  } else {
+   links[i].firstChild.removeAttribute("class");
+  }
+ }
+ backToTop();
+}
+
+
+
 function initContent(frag, contents) {
  for (e in contents) {
   if (contents[e].tag == frag) {
-   var page = contents[e].content;
-   var sideNavId = contents[e].id;
+   loadMain(contents[e].content);
+   document.title = ` Kodiranje | ${ contents[e].pagetitle } ` // sets page title
+   document.querySelector('#metadesc').content = contents[e].pagedesc; // dinamički updejtuje sadržaj meta taga za svaku stranicu
+   loadSideNav(contents[e].id);
    break;
   }
  }
- loadMain(page);
- loadSideNav(sideNavId);
+
  setActive(frag);
+}
+
+
+// resizes text area containing code to fit the contents
+function textAreaSizer() {
+ var test = document.getElementsByClassName("code_snippet");
+ for (let i = 0; i < test.length; i ++) {
+  let scroll = test[i].scrollHeight;
+  test[i].style.height = test[i].scrollHeight + 'px';
+ }
+}
+
+
+
+function detailsFixer() {
+  const detailsOpen = Array.from(document.getElementsByTagName("details"));
+  for (em in detailsOpen) {
+    detailsOpen[em].addEventListener("toggle", ()=> {
+      textAreaSizer();
+      closeDetails(detailsOpen);
+      // add func that will close all open details 
+      // it should have open attribute, with statuses open and false
+    });
+  }
+}
+
+function closeDetails (detailsOpen) {
+  //
 }
 
 function loadMain(page) {
  const mainContent = document.getElementById("placeholder");
  mainContent.innerHTML = page;
  textAreaSizer();
+ detailsFixer();
 }
+
+
 
 window.addEventListener("hashchange", pageInit);
 
-function loadSideNav(sideNavId) {                   // links                                                    items
- const sideNav = [["mp", ["#recnik", "#brauzeri_i_editori", "#ada", "#turing", ["#vitalik_buterin"], "#timbl", "#brendan_eich"], ["Rečnik", "Softver", "Ada Lovlejs", "Alan Turing", ["Vitalik Buterin"], "TimBL", "Brendan Ajk",]],
-                  ["hc", ["#uvod_u_HTML_i_CSS", "#struktura_HTML_stranice", "#prvi_sajt", "#linkovi", "#uvod_u_CSS", "#predah", "#boje", "#uvod_u_slike"], ["Uvod", "HTML struktura", "Prvi Sajt", "Linkovi", "Uvod u CSS", "Predah", "Boje", "Uvod u slike"]],
-                  ["gc", ["#uvod_u_cmd", "#cd_md_rd"], ["Uvod u cmd", "CD, MD, RD"]],
-                  //["js", ["#uvod_u_js"], ["Uvod u JS"]]
+
+
+// functionality of sideway nav
+function leftRightNav(links) {
+  let levo = document.getElementById("left");
+  let desno = document.getElementById("right");
+  let current = location.hash;
+  let ind = links.indexOf(current)
+  if (ind == 0) {
+    levo.style.display = "none";
+  }
+  else {
+    levo.innerHTML = ""; // important otherwise the pr.'s add up
+    let backlink = document.createElement("a");
+    levo.appendChild(backlink);
+    backlink.setAttribute("href", links[ind-1]);
+    backlink.setAttribute("rel", "prev chapter");
+    backlink.setAttribute("rev", "next");
+    backlink.innerHTML = "pr.";
+  }
+  if (ind == (links.length -1)) {
+    desno.style.display = "none";
+  }
+  else {
+    desno.innerHTML = "";
+    let forlink = document.createElement("a");
+    desno.appendChild(forlink);
+    forlink.setAttribute("href", links[ind+1]);
+    forlink.setAttribute("rev", "prev");
+    forlink.setAttribute("rel", "next chapter");
+    forlink.innerHTML = "sl.";
+  }
+}
+
+
+
+function loadSideNav(sideNavId) {
+ const sideNav = [["mp", [         // links
+                         "#recnik",
+                         "#brauzeri_i_editori",
+                         "#HTML_u_JSON_izbegavanje_karaktera",
+                         "#ada",
+                         "#turing",
+                         "#brendan_eich",
+                         "#vitalik_buterin",
+                         "#grace_hopper",
+                         "#timbl",
+                         "#hedy_lamarr",
+                         ], [      //items
+                                 "Rečnik",
+                                 "Softver",
+                                 "HTML u JSON",
+                                 "Ada Lovlejs",
+                                 "Alan Turing",
+                                 "Brendan Ajk",
+                                 "Vitalik Buterin",
+                                 "Grejs Hoper",
+                                 "TimBL",
+                                 "Hedi Lamar",
+                                 ]],
+                  ["hc", [         // links
+                          "#uvod_u_HTML_i_CSS",
+                          "#struktura_HTML_stranice",
+                          "#prvi_sajt",
+                          "#linkovi",
+                          "#uvod_u_CSS",
+                          "#osnovni_css",
+                          "#css_linkova",
+                          "#predah",
+                          "#uvod_u_slike",
+                          "#klase_i_id-ijevi",
+                          "#divovi_i_spanovi",
+                          "#meta",
+                          ], [      //items
+                                 "Uvod",
+                                 "HTML struktura",
+                                 "Prvi Sajt",
+                                 "Linkovi",
+                                 "Uvod u CSS",
+                                 "Osnovni CSS",
+                                 "CSS Linkova",
+                                 "Predah",
+                                 "Uvod u slike",
+                                 "Klase i Id-ijevi",
+                                 "Divovi i Spanovi",
+                                 "Meta tagovi",
+                                 ]],
+                  ["gc", [         // links
+                          "#uvod_u_cmd",
+                          "#cd_md_rd",
+                          "#uvod_u_git",
+                          "#git_instalacija",
+                          ], [      //items
+                                 "Uvod u cmd",
+                                 "CD, MD, RD",
+                                 "Uvod u Git",
+                                 "Instalacija, Podešavanja",
+                                 ]],
+                  ["js", [         // links
+                         "#uvod_u_js",
+                         "#js_varijable_i_konstante",
+                         "#js_funkcije",
+                         ], [      //items
+                                 "Uvod u JS",
+                                 "Varijable i konstante",
+                                 "Funkcije",
+                                 ]]
                   ];
+
+// clears up previously selected top nav item
  for (let i = 1; i < sideNav.length; i ++) {
   document.getElementById(sideNav[i][0]).removeAttribute("class");
  }
- let pos = document.getElementById("side-nav");
- var sideMenu = '<ul id="side-menu">';
+
  for (e in sideNav) {
   if (sideNav[e][0] == sideNavId) {
-   var links = sideNav[e][1],
-       items = sideNav[e][2];
-   for (el in links) {
-    sideMenu = sideMenu + '<a href="' + links[el] + '"><li>' + items[el] + '</li></a>';
-   }
+   getSideNavList(sideNav[e])
+   var links = sideNav[e][1]
   }
  }
- pos.innerHTML = sideMenu + '</ul>'
+
  let leftright = document.getElementsByClassName("leftright");
+ 
  if(sideNavId != "mp") {
   document.getElementById(sideNavId).setAttribute("class", "selected");
   for(var i = 0; i < 2; i++) {
@@ -139,63 +322,120 @@ function loadSideNav(sideNavId) {                   // links                    
     leftright[i].style.display = "none";
   }
  }
- leftRight(links);
+ leftRightNav(links);
 }
 
-function leftRight(links) {
-  let levo = document.getElementById("left");
-  let desno = document.getElementById("right");
-  let current = location.hash;
-  let ind = links.indexOf(current)
-  if (ind == 0) {
-    levo.style.display = "none";
-  }
-  else {
-    levo.innerHTML = "";
-    let backlink = document.createElement("a");
-    levo.appendChild(backlink);
-    backlink.setAttribute("href", links[ind-1]);
-    backlink.innerHTML = "pr.";
-  }
-  if (ind == (links.length -1)) {
-    desno.style.display = "none";
-  }
-  else {
-    desno.innerHTML = "";
-    let forlink = document.createElement("a");
-    desno.appendChild(forlink);
-    forlink.setAttribute("href", links[ind+1]);
-    forlink.innerHTML = "sl.";
-  }
-   levo.onclick = function() {
-    window.scrollTo(0, 0);
-   } 
-   desno.onclick = function() {
-    window.scrollTo(0, 0);
-   }
-}
 
-function setActive(frag) {
- var links = document.querySelectorAll("#side-nav a");
- let i, link, pageName;
- for (i = 0; i < links.length; i++) {
-  link = links[i];
-  pageName = link.getAttribute("href").substr(1);
-  if (pageName === frag) {
-   link.firstChild.setAttribute("class", "active");
-  } else {
-   link.firstChild.removeAttribute("class");
-  }
+
+// populates side-nav 
+function getSideNavList(list) {
+ let sideMenu = '<ul id="side-menu">';
+ var [ id, links, items ] = list;
+ for (el in links) {
+  sideMenu = sideMenu + '<a href="' + links[el] + '" rel="chapter"><li>' + items[el] + '</li></a>';
  }
+ document.getElementById("side-nav").innerHTML = sideMenu + '</ul>';
+ //leftRightNav(links);
 }
 
-function textAreaSizer() {
 
- var test = document.getElementsByClassName("code_snippet");
- //console.log(test);
- for (let i = 0; i < test.length; i ++) {
-  let scroll = test[i].scrollHeight;
-  test[i].style.height = test[i].scrollHeight + 'px';
-  //console.log(scroll);
+
+// fixes top bar to the top of the page once scrolled pass logo
+function bindMenu() {
+ let topMenu = document.getElementById("header-bottom"),
+     home = document.getElementById("backhome"),
+     smallDevice = window.innerWidth;
+
+  if (document.body.scrollTop >= 70 || document.documentElement.scrollTop >= 70) {
+   topMenu.className = "fixed-top";
+   home.style.visibility = "visible";
+  }
+  else {
+   topMenu.className = "";
+   home.style.visibility = "hidden";
   }
 }
+
+window.onscroll = ()=> bindMenu();
+
+
+
+// adds copy/date to the footer
+(copid = function() {
+ let d = new Date().getFullYear();
+ document.querySelector('#copydate').innerHTML = "&copy; ČĎŠ " + d;
+})();
+
+
+
+// JSON escape tool button 1
+function escapeBad() {
+ let insertion = document.getElementById("insertf").value;
+ const bad = [
+              "ć", 
+              "đ", 
+              "š", 
+              "ž", 
+              "č", 
+              "Š", 
+              "Ć", 
+              "Đ", 
+              "Ž", 
+              "Č", 
+              "Ď", 
+              "ô", 
+              "λ",
+              //"&", 
+              '"', 
+              "(\\r\\n|\\r|\\n)", 
+              "\\/", 
+              ];
+ const good = [
+              "\\u0107", // ć
+              "\\u0111", // đ
+              "\\u0161", // š
+              "\\u017E", // ž
+              "\\u010D", // č
+              "\\u0160", // Š
+              "\\u0106", // Ć
+              "\\u0110", // Đ
+              "\\u017D", // Ž
+              "\\u010C", // Č
+              "\\u010E", // Ď
+              "\\u00F4", // ô
+              "\\u03BB", // λ
+              //"&amp;", 
+              '\\\"', 
+              "\\r\\n", 
+              "\\/", 
+              ];
+ for(let i = 0; i < bad.length; i++) {
+  insertion = insertion.replace(RegExp(bad[i], 'g'), good[i]);
+ }
+  
+ var res = document.getElementById("result");
+ res.value = `${insertion}`;
+}
+
+
+// JSON escape tool button 2
+function clearField() {
+ document.getElementById("insertf").value = "";
+ document.getElementById("result").value = "";
+}
+
+
+
+
+/*(test = function(){
+ const target = document.querySelector('#logo-text');
+ const split = Array.from(target.textContent.substring(3,13));
+ //const split =
+ let writeKodiranje_ = '<text x="40" y="48">';
+ let i = 0;
+ 
+ }, 1000);
+ writeKodiranje_ += '</text>';
+ 
+ console.log(split);
+})();*/
--- sw.js
+++ sw.js
@@ -1,16 +1,17 @@
+let staticCacheName = 'kodiranje';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('kodiranje').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
+        '/',
         '../../hikeislife.kodiranje.io/other/manifest.webmanifest',
 
         '../../hikeislife.kodiranje.io',
         '../../hikeislife.kodiranje.io/index.html',
 
         '../../hikeislife.kodiranje.io/styles/style.css',
-        //'../../hikeislife.kodiranje.io/styles/dt-style.css',
 
-        //'../../hikeislife.kodiranje.io/other/',
         '../../hikeislife.kodiranje.io/other/cog192.png',
         '../../hikeislife.kodiranje.io/other/cog512.png',
         '../../hikeislife.kodiranje.io/other/cog48.png',
@@ -19,7 +20,9 @@ self.addEventListener('install', function(event) {
 
         '../../hikeislife.kodiranje.io/app.js',
         '../../hikeislife.kodiranje.io/sw.js',
-        '../../hikeislife.kodiranje.io/cog.png'
+        '../../hikeislife.kodiranje.io/cog.png',
+
+        'https://fonts.googleapis.com/css?family=Cutive+Mono'
       ]);
     })
   );
@@ -38,7 +41,7 @@ self.addEventListener('fetch', function(event) {
         // and serve second one
         let responseClone = response.clone();
         
-        caches.open('kodiranje').then(function (cache) {
+        caches.open(staticCacheName).then(function (cache) {
           cache.put(event.request, responseClone);
         });
         return response;
@@ -48,3 +51,97 @@ self.addEventListener('fetch', function(event) {
     }
   }));
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('kodiranje-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return cashe.delete(cacheName);
+        })
+      );
+    })
+  );
+  
+  //cashes.delete('oldkodiranje')
+});
+
+
+
+
+/*document.querySelector('.cache-article').addEventListener('click', function(event) {
+  event.preventDefault();
+  var id = this.dataset.articleId;
+  caches.open('mysite-article-' + id).then(function(cache) {
+    fetch('/get-article-urls?id=' + id).then(function(response) {
+      // /get-article-urls returns a JSON-encoded array of
+      // resource URLs that a given article depends on
+      return response.json();
+      console.log(response.json());
+    }).then(function(urls) {
+      cache.addAll(urls);
+    });
+  });
+});*/
+
+/*var CACHE_VERSION = 1;
+
+// Shorthand identifier mapped to specific versioned cache.
+var CURRENT_CACHES = {
+  font: 'font-cache-v' + CACHE_VERSION
+};
+
+self.addEventListener('activate', function(event) {
+  var expectedCacheNames = Object.values(CURRENT_CACHES);
+
+  // Active worker won't be treated as activated until promise
+  // resolves successfully.
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (!expectedCacheNames.includes(cacheName)) {
+            console.log('Deleting out of date cache:', cacheName);
+            
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    
+    // Opens Cache objects that start with 'font'.
+    caches.open(CURRENT_CACHES['font']).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found response in cache:', response);
+
+          return response;
+        }
+
+        console.log('Fetching request from the network');
+
+        return fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+        });
+      }).catch(function(error) {
+        
+        // Handles exceptions that arise from match() or fetch().
+        console.error('Error in fetch handler:', error);
+
+        throw error;
+      });
+    })
+  );
+});*/
