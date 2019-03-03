88e9fe3bc9d191f6cba5a85b649699dce3dbb5cb
hikeislife <hikeislife@users.noreply.github.com>
2019-03-03 20:44:23 +0100

    missing comma

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ function loadSideNav(sideNavId) {
                          "#brendan_eich",
                          "#vitalik_buterin",
                          "#grace_hopper",
-                         "#lajnus_torvalds"
+                         "#lajnus_torvalds",
                          "#maja_pantic",
                          "#mika_alas",
                          "#ryan_dahl",
@@ -256,7 +256,7 @@ function loadSideNav(sideNavId) {
                                  "Brendan Ajk",
                                  "Vitalik Buterin",
                                  "Grejs Hoper",
-                                 "Lajnus Torvalds"
+                                 "Lajnus Torvalds",
                                  "Maja Pantić",
                                  "Mihailo Petrović Alas",
                                  "Rajan Dal",
