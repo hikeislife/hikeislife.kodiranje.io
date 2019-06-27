f051220fc7cb2febcf466ca41e44c2e0f43d302c
hikeislife <hikeislife@users.noreply.github.com>
2019-06-27 19:37:08 +0200

    sw update

--- sw.js
+++ sw.js
@@ -6,15 +6,15 @@ const precacheFiles = [
         '../../hikeislife.kodiranje.io',
         '../../hikeislife.kodiranje.io/index.html',
 
-        '../../hikeislife.kodiranje.io/styles/style.css',
+        'hikeislife.kodiranje.io/styles/style.css',
 
         '../../hikeislife.kodiranje.io/other/cog192.png',
         '../../hikeislife.kodiranje.io/other/cog512.png',
         '../../hikeislife.kodiranje.io/other/cog48.png',
 
-        '../../hikeislife.kodiranje.io/js/content.json',
+        'hikeislife.kodiranje.io/js/content.json',
 
-        '../../hikeislife.kodiranje.io/app.js',
+        'hikeislife.kodiranje.io/app.js',
         '../../hikeislife.kodiranje.io/sw.js',
         '../../hikeislife.kodiranje.io/cog.png',
 
