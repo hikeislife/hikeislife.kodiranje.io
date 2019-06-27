d48db604b16e0ea7405d90d3b6916122b2a6f504
hikeislife <hikeislife@users.noreply.github.com>
2019-06-27 22:05:47 +0200

    sw update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 let CACHE = "cache";
 const precacheFiles = [
-        '/hikeislife.kodiranje.io/'
+        '/'
 ];
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
